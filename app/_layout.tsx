58689a491fef931d8cf2babc5bb5fc4f980a7020
5yndr0m <sdilanjana21@gmail.com>
2025-05-28 22:09:00 +0530

    feat: change stack to slot

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { Provider } from "react-native-paper"
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Provider>
+      <Slot />
+    </Provider>
+  );
 }

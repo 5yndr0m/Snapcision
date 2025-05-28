13d3746f57d7651d0c718bfc33876c0ae90230b1
5yndr0m <sdilanjana21@gmail.com>
2025-05-28 22:54:46 +0530

    feat: link albums

--- app/index.tsx
+++ app/index.tsx
@@ -2,13 +2,14 @@ import { StyleSheet } from "react-native";
 import { Surface, Text } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Header } from "@/components/navigation/Header";
+import { MediaAlbums } from "@/components/media/MediaAlbums";
 
 export default function Index() {
   return (
     <SafeAreaView style={styles.container}>
       <Header showSettings />
       <Surface style={styles.content}>
-        <Text variant="headlineMedium">Welcome to Snapcision</Text>
+       <MediaAlbums/>
       </Surface>
     </SafeAreaView>
   );
